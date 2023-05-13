08bd91b9b6880e7b54d68e8e0a24d512142ed25b
Timo Schneider <11272488+timoschneider249@users.noreply.github.com>
2023-05-13 17:39:35 +0200

    - set folder path in prompt - create folder by last modified time of file - move file in that folder

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// StringPrompt asks for a string value using the label
+func StringPrompt(label string) string {
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stderr, label+" ")
+		s, _ = r.ReadString('\n')
+		if s != "" {
+			break
+		}
+	}
+	return strings.TrimSpace(s)
+}
+
+func main() {
+	name := StringPrompt("Please select a Directory. \n")
+	fmt.Printf("Chose directory: %s!\n", name)
+	readDir(name)
+}
+
+func readDir(path string) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		filename := filepath.Join(path, file.Name())
+		if !file.IsDir() {
+			dat, err := os.ReadFile(filename)
+			checkErr(err)
+			fmt.Print(http.DetectContentType(dat) + " : ")
+			format := file.ModTime().Format("2006-02-01")
+			fileName := file.Name()
+			fmt.Println(fileName, format)
+			folderPath := filepath.Join(path, format)
+			oldPath := filepath.Join(path, fileName)
+			err = os.MkdirAll(folderPath, os.ModePerm)
+			checkErr(err)
+			newPath := filepath.Join(folderPath, fileName)
+			fmt.Println(oldPath)
+			fmt.Println(newPath)
+			err = os.Rename(oldPath, newPath)
+			checkErr(err)
+		}
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
